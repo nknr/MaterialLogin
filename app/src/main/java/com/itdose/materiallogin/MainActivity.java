583f5e69cf07103d40fb4e28542af6a794ba7a18
neeraj.kumar@itdoseinfo.com <NkSidd147532>
2019-06-04 18:04:30 +0530

    Initial commit

---
+++ app/src/main/java/com/itdose/materiallogin/MainActivity.java
@@ -0,0 +1,21 @@
+package com.itdose.materiallogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSignUp(View view){
+        startActivity(new Intent(this,SignUpActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+    }
+}
---
+++ app/src/main/java/com/itdose/materiallogin/SignUpActivity.java
@@ -0,0 +1,22 @@
+package com.itdose.materiallogin;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+}
